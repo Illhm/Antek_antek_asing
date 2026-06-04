9196a482361d4f8f752487597e939a7a12f3caa9
Illhm <gusmif@ilhamxander.web.id>
2026-06-05 05:15:42 +0700

    Delete migration2.sql

--- migration2.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `proxy_mappings`
-ADD COLUMN `user_id` int(11) NOT NULL AFTER `rid`,
-ADD COLUMN `ip_prefix` varchar(45) NOT NULL AFTER `original_url`,
-ADD COLUMN `ua_hash` varchar(64) NOT NULL AFTER `ip_prefix`,
-ADD COLUMN `device_fingerprint` varchar(255) NOT NULL AFTER `ua_hash`;
